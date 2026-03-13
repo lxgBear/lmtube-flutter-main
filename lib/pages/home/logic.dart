853d624ccade603c03dcfc4f62f8b7f0bcf02656
lxgBear <151456.@QAZ>
2026-03-13 16:12:14 +0800

    chore: auto commit 2 files (2026-03-13)

--- lib/pages/home/logic.dart
+++ lib/pages/home/logic.dart
@@ -1,15 +1,109 @@
 import 'package:get/get.dart';
 import 'package:mobile/global/logic.dart';
+import 'package:mobile/http/recommend.dart';
 import 'package:mobile/routes.dart';
+
+import '../../http/net.dart';
 
 class HomeLogic extends GetxController {
   final global = Get.find<GlobalLogic>().state;
+  List<HimalayaSubItemInfo> rankList = [
+    HimalayaSubItemInfo(
+      title: '推荐',
+      state: true,
+    ),
+    HimalayaSubItemInfo(
+      title: '澜湄视听周',
+      state: false,
+    ),
+    HimalayaSubItemInfo(
+      title: '党政专栏',
+      state: false,
+    ),
+    HimalayaSubItemInfo(
+      title: '澜湄观察',
+      state: false,
+    )
+  ];
+
+  List<TitleContent> titleContent = [
+    TitleContent(
+      title: '坚持不懈用习近平新时代中国特色社会主义思想凝心铸魂',
+      url: 'https://via.placeholder.com/359x188',
+      publisher: '澜湄观察',
+      releaseTime: '14分钟前',
+    ),
+    TitleContent(
+      title: '云南省与教育部签署战略合作协议',
+      url: 'https://via.placeholder.com/359x188',
+      publisher: '澜湄观察',
+      releaseTime: '14分钟前',
+    ),
+    TitleContent(
+      title: '云南省重大科技专项“发酵蔬菜加工共性关键技术研究与产业化”通过验收',
+      url: 'https://via.placeholder.com/359x188',
+      publisher: '澜湄观察',
+      releaseTime: '14分钟前',
+    ),
+  ];
 
   @override
   void onReady() {
+    print("xnssxpmxopmxpsmxp");
+
     if (global.accessToken == "") {
       Get.toNamed(HRoutes.login);
     }
+    handleUserList();
     super.onReady();
   }
+
+  void clickss(e) {
+    for (var i = 0; i < rankList.length; i++) {
+      rankList[i].state = false;
+    }
+    rankList[e].state = true;
+    update();
+  }
+
+  Future<void> handleUserList() async {
+    try {
+      var r = await fetch<Recommend>(
+        "v1/recommend/paging?page=0&count=20",
+        method: "GET",
+        silent: false,
+
+
+      );
+      print(r);
+    } catch (_) {}
+  }
+}
+
+class HimalayaSubItemInfo {
+  HimalayaSubItemInfo({
+    required this.title,
+    this.state = false,
+  });
+
+  ///按钮名称
+  String title;
+  bool state;
+}
+
+class TitleContent {
+  TitleContent({
+    required this.title,
+    required this.url,
+    required this.publisher,
+    required this.releaseTime,
+  });
+
+  ///标题
+  String title;
+
+  ///子列表
+  String url;
+  String publisher;
+  String releaseTime;
 }
--- lib/pages/home/view.dart
+++ lib/pages/home/view.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
-import 'package:mobile/generated/locales.g.dart';
-import 'package:mobile/routes.dart';
 
+import '../../generated/locales.g.dart';
+import '../../styles/theme.dart';
 import 'logic.dart';
 
 class HomePage extends StatelessWidget {
@@ -14,17 +14,281 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var theme = Theme.of(context);
-
     return AnnotatedRegion<SystemUiOverlayStyle>(
       value: SystemUiOverlayStyle(
         systemNavigationBarColor: theme.colorScheme.surface,
       ),
       child: Scaffold(
-        body: Center(
-            child: GestureDetector(
-                onTap: () => Get.toNamed(HRoutes.login),
-                child: Text(LocaleKeys.app.tr))),
-      ),
+          appBar: AppBar(
+            title: Text("asnxisnxi"),
+          ),
+          body: SingleChildScrollView(
+            child: Container(
+                child: Column(
+              children: [
+                _buildCardBg(theme),
+                Image.network(
+                  "https://via.placeholder.com/359x188",
+                ),
+                Container(
+                  margin: EdgeInsets.only(left: 8, right: 8, top: 8),
+                  child: Column(
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: _listView(theme),
+                      ),
+                      _card(theme),
+                      Padding(padding: EdgeInsets.only(top: 20)),
+                      _lists(),
+                    ],
+                  ),
+                )
+              ],
+            )),
+          )),
+    );
+  }
+
+
+  Widget _lists() {
+    return Container(
+        child: Column(children: [
+          Row(
+            children: [
+              Container(
+                width: 140,
+                height: 100,
+                decoration: ShapeDecoration(
+                  image: DecorationImage(
+                    image: NetworkImage("https://via.placeholder.com/140x100"),
+                    fit: BoxFit.fill,
+                  ),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8)),
+                ),
+              ),
+              Padding(padding: EdgeInsets.only(left: 8)),
+              Container(
+                color: Colors.red,
+                  height: 92,
+               child: const Column(
+                 mainAxisSize: MainAxisSize.min,
+                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                 crossAxisAlignment: CrossAxisAlignment.start,
+                 children: [
+                   Text("习近平在浙江金华市考察调研"),
+                   Text("澜湄视听 . 15分钟前"),
+                 ],
+               ),
+              )
+            ],
+          )   ,
+          Padding(padding: EdgeInsets.only(top: 8)),
+          Row(
+            children: [
+              Container(
+                width: 140,
+                height: 100,
+                decoration: ShapeDecoration(
+                  image: DecorationImage(
+                    image: NetworkImage("https://via.placeholder.com/140x100"),
+                    fit: BoxFit.fill,
+                  ),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8)),
+                ),
+              ),
+              Padding(padding: EdgeInsets.only(left: 8)),
+              Container(
+                color: Colors.red,
+                  height: 92,
+               child: const Column(
+                 mainAxisSize: MainAxisSize.min,
+                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                 crossAxisAlignment: CrossAxisAlignment.start,
+                 children: [
+                   Text("习近平在浙江金华市考察调研"),
+                   Text("澜湄视听 . 15分钟前"),
+                 ],
+               ),
+              )
+            ],
+          )
+        ]));
+  }
+
+  Widget _card(ThemeData theme) {
+    return Container(
+      child: Column(children: [
+        Image.network(
+          'https://via.placeholder.com/140x100',
+          fit: BoxFit.fill,
+          width: 359,
+          height: 188,
+        ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Padding(
+              padding: EdgeInsets.only(top: 8),
+            ),
+            Text("有一种叫云南的生活·我家乡村美，当一群年轻人决定回乡",
+                style: theme.textTheme.labelMedium?.copyWith(
+                  color: theme.colorScheme.onSurfaceVariant,
+                  fontSize: 16,
+                  fontWeight: FontWeight.w400,
+                )),
+            const Padding(
+              padding: EdgeInsets.only(top: 8),
+            ),
+            const Text("澜湄视听 . 15分钟前")
+          ],
+        ),
+      ]),
+    );
+  }
+
+  List<Widget> _listView(ThemeData theme) {
+    return logic.titleContent
+        .map((e) => Container(
+              margin: EdgeInsets.only(top: 8, bottom: 12),
+              child: Column(
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(e.title,
+                          style: theme.textTheme.labelMedium?.copyWith(
+                              color: theme.colorScheme.onSurface,
+                              fontSize: 16,
+                              fontWeight: FontWeight.w400)),
+                      const Padding(padding: EdgeInsets.only(top: 8)),
+                      Text.rich(TextSpan(
+                        children: [
+                          TextSpan(
+                            text: LocaleKeys.commonly_used_major.tr,
+                            style: theme.textTheme.labelMedium?.copyWith(
+                                color: theme.colorScheme.primary,
+                                fontSize: 12,
+                                fontWeight: FontWeight.w400),
+                          ),
+                          TextSpan(
+                              text: e.publisher,
+                              style: theme.textTheme.labelMedium?.copyWith(
+                                color: theme.colorScheme.onSurface,
+                                fontSize: 12,
+                                fontWeight: FontWeight.w400,
+                              )),
+                          TextSpan(
+                              text: ' . ${e.releaseTime}',
+                              style: theme.textTheme.labelMedium?.copyWith(
+                                color: theme.colorScheme.onSurface,
+                                fontSize: 12,
+                                fontWeight: FontWeight.w400,
+                              )),
+                        ],
+                      ))
+                    ],
+                  )
+                ],
+              ),
+            ))
+        .toList();
+  }
+
+  Widget _buildCardBg(ThemeData theme) {
+    return Container(
+      margin: EdgeInsets.only(left: 10, bottom: 8),
+      child: Row(children: [
+        GestureDetector(
+            onTap: () => logic.clickss(0),
+            child: GetBuilder<HomeLogic>(
+              builder: (HomeLogic controller) {
+                return Container(
+                  padding:
+                      EdgeInsets.only(left: 16, right: 16, top: 6, bottom: 6),
+                  margin: EdgeInsets.only(left: 8),
+                  decoration: ShapeDecoration(
+                    color: logic.rankList[0].state
+                        ? theme.colorScheme.secondaryContainer
+                        : HThemeData.getSurfaceContainer(theme),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(4)),
+                  ),
+                  child: Text('${logic.rankList[0].title}',
+                      style: theme.textTheme.labelMedium?.copyWith(
+                        color: theme.colorScheme.onSurface,
+                      )),
+                );
+              },
+            )),
+        GestureDetector(
+            onTap: () => logic.clickss(1),
+            child: GetBuilder<HomeLogic>(
+              builder: (HomeLogic controller) {
+                return Container(
+                  padding:
+                      EdgeInsets.only(left: 16, right: 16, top: 6, bottom: 6),
+                  margin: EdgeInsets.only(left: 8),
+                  decoration: ShapeDecoration(
+                    color: logic.rankList[1].state
+                        ? theme.colorScheme.secondaryContainer
+                        : HThemeData.getSurfaceContainer(theme),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(4)),
+                  ),
+                  child: Text('${logic.rankList[1].title}',
+                      style: theme.textTheme.labelMedium?.copyWith(
+                        color: theme.colorScheme.onSurface,
+                      )),
+                );
+              },
+            )),
+        GestureDetector(
+            onTap: () => logic.clickss(2),
+            child: GetBuilder<HomeLogic>(
+              builder: (HomeLogic controller) {
+                return Container(
+                  padding:
+                      EdgeInsets.only(left: 16, right: 16, top: 6, bottom: 6),
+                  margin: EdgeInsets.only(left: 8),
+                  decoration: ShapeDecoration(
+                    color: logic.rankList[2].state
+                        ? theme.colorScheme.secondaryContainer
+                        : HThemeData.getSurfaceContainer(theme),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(4)),
+                  ),
+                  child: Text('${logic.rankList[2].title}',
+                      style: theme.textTheme.labelMedium?.copyWith(
+                        color: theme.colorScheme.onSurface,
+                      )),
+                );
+              },
+            )),
+        GestureDetector(
+            onTap: () => logic.clickss(3),
+            child: GetBuilder<HomeLogic>(
+              builder: (HomeLogic controller) {
+                return Container(
+                  padding:
+                      EdgeInsets.only(left: 16, right: 16, top: 6, bottom: 6),
+                  margin: EdgeInsets.only(left: 8),
+                  decoration: ShapeDecoration(
+                    color: logic.rankList[3].state
+                        ? theme.colorScheme.secondaryContainer
+                        : HThemeData.getSurfaceContainer(theme),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(4)),
+                  ),
+                  child: Text('${logic.rankList[3].title}',
+                      style: theme.textTheme.labelMedium?.copyWith(
+                          color: theme.colorScheme.onSurface, fontSize: 14)),
+                );
+              },
+            ))
+      ]),
     );
   }
 }
